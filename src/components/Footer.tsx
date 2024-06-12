10499bceb9b7b82eabed816b9d6d12eaa3aeccd9
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-06-12 12:46:02 -0700

    cleanup

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <div className="flex justify-center bg-neutral-900 p-3">
-            <p>END</p>
-        </div>
-    );
-};
-
-export default Footer;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,10 +3,7 @@ const Home = () => {
         <div>
             <h1 className="mb-10 font-bold text-6xl">Home</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                voluptatibus animi dignissimos inventore modi ipsa repudiandae,
-                veniam enim. Itaque quod corrupti, similique voluptas sequi ut
-                eveniet porro! Vitae, assumenda quidem.
+                Coming soon...
             </p>
         </div>
     );
