fbab3e0be493629095eff771ae0772965dea7ae6
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-06-12 13:12:31 -0700

    add resume link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,8 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoLocationSharp } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
 import { BsDot } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { HiDocumentText } from "react-icons/hi";
 
 const Sidebar = () => {
     const nav = useNavigate();
@@ -29,26 +31,50 @@ const Sidebar = () => {
                     )}
                 </div>
             </div>
-            <div className="flex justify-around">
-                <div>
-                    <a
-                        href="https://github.com/jnarcher"
-                        target="_blank"
-                        className="flex items-center gap-3 hover:bg-neutral-800 px-3 py-1 rounded-md hover:text-white transition-colors"
-                    >
-                        <FaGithub />
-                        <span className="text-lg">Github</span>
-                    </a>
+            <div className="flex flex-col">
+                <div className="flex justify-around">
+                    <div>
+                        <a
+                            href="https://github.com/jnarcher"
+                            target="_blank"
+                            className="flex items-center gap-3 hover:bg-neutral-800 px-3 py-1 rounded-md hover:text-white transition-colors"
+                        >
+                            <FaGithub />
+                            <span className="text-lg">Github</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="https://www.linkedin.com/in/jalennarcher/"
+                            target="_blank"
+                            className="flex items-center gap-3 hover:bg-neutral-800 px-3 py-1 rounded-md hover:text-white transition-colors"
+                        >
+                            <FaLinkedin />
+                            <span className="text-lg">LinkedIn</span>
+                        </a>
+                    </div>
                 </div>
-                <div>
-                    <a
-                        href="https://www.linkedin.com/in/jalennarcher/"
-                        target="_blank"
-                        className="flex items-center gap-3 hover:bg-neutral-800 px-3 py-1 rounded-md hover:text-white transition-colors"
-                    >
-                        <FaLinkedin />
-                        <span className="text-lg">LinkedIn</span>
-                    </a>
+                <div className="flex justify-around">
+                    <div>
+                        <a
+                            href="mailto:jalennarcher@gmail.com"
+                            target="_blank"
+                            className="flex items-center gap-3 hover:bg-neutral-800 px-3 py-1 rounded-md hover:text-white transition-colors"
+                        >
+                            <MdEmail />
+                            <span className="text-lg">Email</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="https://drive.google.com/file/d/17jjyml-QcwUhuWiRRm86OVwWU9G1eVAS/view?usp=sharing"
+                            target="_blank"
+                            className="flex items-center gap-3 hover:bg-neutral-800 px-3 py-1 rounded-md hover:text-white transition-colors"
+                        >
+                            <HiDocumentText />
+                            <span className="text-lg">Resume</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
